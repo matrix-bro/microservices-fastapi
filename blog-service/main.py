5a166ce9ce02b384f8da08e87befb07fd9dbf401
matrix-bro <razmhz11@gmail.com>
2024-02-07 21:40:59 +0545

    feat: adde db connection and create blog creation route

--- blog-service/main.py
+++ blog-service/main.py
@@ -1,11 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from blog.database import engine, SessionLocal
+from blog import schemas, models
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get('/')
 def index():
-    return {"data": {"name": "John Doe"}}
+    return {"data": "Home Page of Blog Site"}
 
-@app.get('/about')
-def about():
-    return {"data": "About Page"}
+
+@app.post('/blog')
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
